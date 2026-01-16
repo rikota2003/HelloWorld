747e356d1c264e0fe35f42b073df4e2d87f59d9a
ricardo <ricardoanton43@gmail.com>
2026-01-16 19:41:21 +0100

    Cambiado nombre de colaborador a ricardo

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
-
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo - ricardo");
+        funcionalidadPaso2();
+    }
 
-	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
-
-	}
-
+    public static void funcionalidadPaso2() {
+        System.out.println("Nueva funcionalidad del Paso 2 añadida correctamente.");
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# HelloWorld
+# HelloWorld Colaborador: ricardo.
